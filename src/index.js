fa5ed9df6e7f26552fef12c46a046bef12d8e725
nanchengyuan <lsd18038215502@163.com>
2022-03-08 14:40:02 +0800

    事件处理完结

--- src/index.js
+++ src/index.js
@@ -584,24 +584,24 @@ reportWebVitals();
 //       super(props);
 //       this.state = {date: new Date()};
 //     }
-  
+
 //     componentDidMount() {
 //       this.timerID = setInterval(
 //         () => this.tick(),
 //         1000
 //       );
 //     }
-  
+
 //     componentWillUnmount() {
 //       clearInterval(this.timerID);
 //     }
-  
+
 //     tick() {
 //       this.setState({
 //         date: new Date()
 //       });
 //     }
-  
+
 //     render() {
 //       return (
 //         <div>
@@ -611,7 +611,7 @@ reportWebVitals();
 //       );
 //     }
 //   }
-  
+
 //   ReactDOM.render(
 //     <Clock />,
 //     document.getElementById('root')
@@ -714,7 +714,7 @@ reportWebVitals();
 //       </div>
 //     );
 //   }
-  
+
 //   ReactDOM.render(
 //     <App />,
 //     document.getElementById('root')
@@ -723,3 +723,116 @@ reportWebVitals();
 //每个 Clock 组件都会单独设置它自己的计时器并且更新它。
 //在React应用中，组件是有状态组件还是无状态组件属于组件实现的细节，它可能会随着时间的推移而改变。你可以在有状态的组件中使用无状态的组件，反之亦然。
 
+
+
+
+//事件处理
+//React 元素的事件处理和 DOM 元素的很相似，但是有一点语法上的不同：
+//1.React 事件的命名采用小驼峰式（camelCase），而不是纯小写。
+//2.使用 JSX 语法时你需要传入一个函数作为事件处理函数，而不是一个字符串。
+//例如，传统的 HTML：
+{/* <button onclick="activateLasers()">
+    Activate Lasers
+</button> */}
+//在 React 中略微不同：
+{/* <button onClick = {activateLasers()}>
+  Activate Lasers
+</button> */}
+
+//在 React 中另一个不同点是你不能通过返回 false 的方式阻止默认行为。你必须显式的使用 preventDefault。例如，传统的 HTML 中阻止表单的默认提交行为，你可以这样写：
+{/* <form onsubmit="console.log('You clicked submit.'); return false">
+  <button type="submit">Submit</button>
+</form> */}
+
+//在 React 中，可能是这样的：
+// function Form() {
+//     function handleSubmit(e) {
+//       e.preventDefault();
+//       console.log('You clicked submit.');
+//     }
+
+//     return (
+//       <form onSubmit={handleSubmit}>
+//         <button type="submit">Submit</button>
+//       </form>
+//     );
+//   }
+//在这里，e 是一个合成事件。React 根据 W3C 规范来定义这些合成事件，所以不需要担心跨浏览器的兼容性问题。React 事件与原生事件不完全相同。
+
+//使用 React 时，你一般不需要使用 addEventListener 为已创建的 DOM 元素添加监听器。事实上，你只需要在该元素初始渲染的时候添加监听器即可。
+
+//当你使用 ES6 class 语法定义一个组件的时候，通常的做法是将事件处理函数声明为 class 中的方法。例如，下面的 Toggle 组件会渲染一个让用户切换开关状态的按钮：
+// class Toggle extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { isToggleOn: true };
+
+//         // 为了在回调中使用“this”，这个绑定是不可必少的
+//         this.handleClick = this.handleClick.bin(this);
+//     }
+
+//     handleClick() {
+//         this.setState(prevState => ({
+//             isToggleOn: !prevState.isToggleOn
+//         }));
+//     }
+
+//     render() {
+//         return (
+//             <button onClick={this.handleClick}>
+//                 {this.state.isToggleOn ? 'No' : "OFF"}
+//             </button>
+//         );
+//     }
+// }
+// ReactDOM.render(
+//     <Toggle />,
+//     document.getElementById('root')
+// );
+//你必须谨慎对待 JSX 回调函数中的 this，在 JavaScript 中，class 的方法默认不会绑定 this。
+//如果你忘记绑定 this.handleClick 并把它传入了 onClick，当你调用这个函数的时候 this 的值为 undefined。
+//这并不是 React 特有的行为；这其实与 JavaScript 函数工作原理有关。通常情况下，如果你没有在方法后面添加 ()，例如 onClick={this.handleClick}，你应该为这个方法绑定 this。
+
+//如果觉得使用 bind 很麻烦，这里有两种方式可以解决。如果你正在使用实验性的 public class fields 语法，你可以使用 class fields 正确的绑定回调函数：
+// class LoggingButton extends React.Component {
+//     //此语法确保 `handleClick` 内的 `this` 已被绑定。
+//     // 注意: 这是 *实验性* 语法。
+//     handleClick = () => {
+//         console.log('this is', this);
+//     }
+
+//     render() {
+//         return (
+//             <button onClick={this.handleClick}>
+//                 Click me
+//             </button>
+//         );
+//     }
+// }
+//Create React App 默认启用此语法。
+
+
+//如果你没有使用 class fields 语法，你可以在回调中使用箭头函数：
+// class LoggingButton extends React.Component {
+//     handleClick() {
+//         console.log('this is', this);
+//     }
+
+//     render() {
+//         // 此语法确保 `handleClick` 内的 `this` 已被绑定。
+//         return (
+//             <button onClick={() => this.handleClick()}>
+//                 Click me
+//             </button>
+//         );
+//     }
+// }
+//此语法问题在于每次渲染 LoggingButton 时都会创建不同的回调函数。在大多数情况下，这没什么问题，
+//但如果该回调函数作为 prop 传入子组件时，这些组件可能会进行额外的重新渲染。我们通常建议在构造器中绑定或使用 class fields 语法来避免这类性能问题。
+
+//向事件处理程序传递参数
+//在循环中，通常我们会为事件处理函数传递额外的参数。例如，若 id 是你要删除那一行的 ID，以下两种方式都可以向事件处理函数传递参数：
+{/* <button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button> */}
+{/* <button onClick={this.deleteRow.bind(this, id)}>Delete Row</button> */}
+//上述两种方式是等价的，分别通过箭头函数和 Function.prototype.bind 来实现。
+//在这两种情况下，React 的事件对象 e 会被作为第二个参数传递。如果通过箭头函数的方式，事件对象必须显式的进行传递，而通过 bind 的方式，事件对象以及更多的参数将会被隐式的进行传递。
